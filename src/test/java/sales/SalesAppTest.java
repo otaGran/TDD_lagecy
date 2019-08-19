dd69dfa9b7b86ff0ff7a0d59a30a073ecfd96df7
OtaGran <ujt2h8@gmail.com>
2019-08-19 08:12:22 +0800

    [alexander][test][should_return_true_when_sales_date_is_valid]

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -2,6 +2,12 @@ package sales;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 public class SalesAppTest {
 
 	@Test
@@ -10,5 +16,25 @@ public class SalesAppTest {
 		SalesApp salesApp = new SalesApp();
 		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
 		
+	}
+
+	@Test
+	public void should_return_true_when_sales_date_is_valid() {
+
+		SalesApp salesApp = new SalesApp();
+		Sales sales = spy(new Sales());
+
+
+		Calendar fromDay = Calendar.getInstance();
+		fromDay.set(Calendar.DATE, fromDay.get(Calendar.DATE) - 100);
+		doReturn(fromDay.getTime()).when(sales).getEffectiveFrom();
+		Calendar toDay = Calendar.getInstance();
+		toDay.set(Calendar.DATE, toDay.get(Calendar.DATE) + 100);
+		doReturn(toDay.getTime()).when(sales).getEffectiveTo();
+
+		boolean res = salesApp.isValidDate(sales);
+
+
+		assertTrue(res);
 	}
 }
